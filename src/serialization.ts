3a4c53c8a4e46e01996d47e0b889dbaee1941ff9
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-05-17 00:26:24 +0300

    Made comparison exported in order to accomodate more complex usage scenarios.

--- src/serialization.ts
+++ src/serialization.ts
@@ -1,5 +1,6 @@
 import { isDeepStrictEqual } from "util";
 import * as common from "@data-heaving/common";
+import * as sql from "@data-heaving/common-sql";
 import * as common_validation from "@data-heaving/common-validation";
 import * as types from "./types";
 
@@ -7,7 +8,7 @@ export type MetaDataFunctionality = common.ObjectStorageFunctionality<types.Seri
 export const performMDWrite = async <TTableID>(
   tables: ReadonlyArray<{
     tableID: TTableID;
-    tableMD: common.TableMetaData;
+    tableMD: sql.TableMetaData;
   }>,
   getTableIDString: (tableID: TTableID) => string,
   { readExistingData, writeNewDataWhenDifferent }: MetaDataFunctionality,
@@ -16,41 +17,76 @@ export const performMDWrite = async <TTableID>(
     readExistingData,
     types.serializedMetaDataTables.decode,
   );
+  const newMD = compareMetaData({
+    previousMetaData: existingMD,
+    latestMetaData: tables,
+    getTableIDString,
+  });
+
+  if (newMD) {
+    await writeNewDataWhenDifferent(newMD);
+  }
+  return newMD;
+};
+
+export interface ComparisonOptions<TTableID> {
+  previousMetaData: types.SerializedExportedTablesMetaData | undefined;
+  latestMetaData:
+    | types.SerializedExportedTablesMetaData
+    | ReadonlyArray<{
+        tableID: TTableID;
+        tableMD: sql.TableMetaData;
+      }>;
+  getTableIDString: (tableID: TTableID) => string;
+}
+
+export const compareMetaData = <TTableID>({
+  previousMetaData,
+  latestMetaData,
+  getTableIDString,
+}: ComparisonOptions<TTableID>) => {
   let tablesMDWasDifferent = false;
-  const metadata = tables.reduce<types.SerializedExportedTablesMetaData>(
-    (tableDictionary, { tableID: tableIDObj, tableMD }) => {
-      const tableID = getTableIDString(tableIDObj);
-      const existingTableMDColumns = existingMD?.[tableID]?.columns;
-      const thisTableColumns = tableMD.columnNames.reduce<
-        types.SerializedExportedTablesMetaData[string]["columns"]
-      >((columns, columnName, columnIndex) => {
-        columns[columnName] = {
-          ...tableMD.columnTypes[columnIndex],
-          isPrimaryKey: columnIndex < tableMD.primaryKeyColumnCount,
+  let metadata: types.SerializedExportedTablesMetaData;
+  if (Array.isArray(latestMetaData)) {
+    metadata = latestMetaData.reduce<types.SerializedExportedTablesMetaData>(
+      (tableDictionary, { tableID: tableIDObj, tableMD }) => {
+        const tableID = getTableIDString(tableIDObj);
+        const existingTableMDColumns = previousMetaData?.[tableID]?.columns;
+        const thisTableColumns = tableMD.columnNames.reduce<
+          types.SerializedExportedTablesMetaData[string]["columns"]
+        >((columns, columnName, columnIndex) => {
+          columns[columnName] = {
+            ...tableMD.columnTypes[columnIndex],
+            isPrimaryKey: columnIndex < tableMD.primaryKeyColumnCount,
+          };
+          return columns;
+        }, {});
+        const existingMDVersion = previousMetaData?.[tableID]?.mdVersion;
+        let mdVersion = existingMDVersion || 0;
+        if (
+          existingTableMDColumns &&
+          !isDeepStrictEqual(thisTableColumns, existingTableMDColumns)
+        ) {
+          // We have noticed a change - increment version
+          ++mdVersion;
+        }
+        tablesMDWasDifferent = mdVersion !== existingMDVersion;
+        tableDictionary[tableID] = {
+          mdVersion,
+          columns: thisTableColumns,
         };
-        return columns;
-      }, {});
-      const existingMDVersion = existingMD?.[tableID]?.mdVersion;
-      let mdVersion = existingMDVersion || 0;
-      if (
-        existingTableMDColumns &&
-        !isDeepStrictEqual(thisTableColumns, existingTableMDColumns)
-      ) {
-        // We have noticed a change - increment version
-        ++mdVersion;
-      }
-      tablesMDWasDifferent = mdVersion !== existingMDVersion;
-      tableDictionary[tableID] = {
-        mdVersion,
-        columns: thisTableColumns,
-      };
-      return tableDictionary;
-    },
-    {},
-  );
+        return tableDictionary;
+      },
+      {},
+    );
+  } else {
+    metadata = latestMetaData;
+    tablesMDWasDifferent = isDeepStrictEqual(previousMetaData, latestMetaData);
+  }
+
   // Notice that we might get table deletion in SQL, but we probably don't want to remove it from MD.
-  if (existingMD) {
-    for (const [tableID, tableMD] of Object.entries(existingMD)) {
+  if (previousMetaData) {
+    for (const [tableID, tableMD] of Object.entries(previousMetaData)) {
       if (!(tableID in metadata)) {
         // There existed table MD which is now gone, but let's not wipe it
         metadata[tableID] = tableMD;
@@ -58,13 +94,13 @@ export const performMDWrite = async <TTableID>(
       }
     }
   }
-  if (tablesMDWasDifferent) {
-    await writeNewDataWhenDifferent(metadata);
-  }
-  return {
-    tablesMDWasDifferent,
-    metadata,
-  };
+
+  return tablesMDWasDifferent ? metadata : undefined;
 };
 
-export * from "./types";
+// temp-fix for this: https://github.com/microsoft/TypeScript/issues/17002
+declare global {
+  interface ArrayConstructor {
+    isArray(arg: ReadonlyArray<any> | any): arg is ReadonlyArray<any>; // eslint-disable-line
+  }
+}
