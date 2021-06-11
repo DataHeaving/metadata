5ebbcb085148837cc43dfce6b88f3f12a226fd96
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-06-11 13:27:07 +0300

    Added fix and test for situation when new table was not always detected.

--- sql/src/tests/serialization.spec.ts
+++ sql/src/tests/serialization.spec.ts
@@ -76,3 +76,81 @@ test("Previously existing table does not affect result when latest is array with
     "Metadata must be considered different, and include non-existant table",
   );
 });
+
+test("New table detected correctly", (t) => {
+  const previousMetaData: spec.ComparisonOptions<string>["previousMetaData"] = {
+    table: {
+      mdVersion: 0,
+      columns: {
+        col: {
+          typeName: "INT",
+          isNullable: false,
+          isPrimaryKey: true,
+          maxLength: 0,
+          precision: 0,
+          scale: 0,
+        },
+      },
+    },
+    anotherTable: {
+      mdVersion: 0,
+      columns: {},
+    },
+  };
+  const latestMetaData: spec.ComparisonOptions<string>["latestMetaData"] = [
+    {
+      tableID: "new-table",
+      tableMD: {
+        columnNames: ["col"],
+        columnTypes: [
+          {
+            typeName: "INT",
+            isNullable: false,
+            maxLength: 0,
+            precision: 0,
+            scale: 0,
+          },
+        ],
+        isCTEnabled: true,
+        primaryKeyColumnCount: 1,
+      },
+    },
+    {
+      tableID: "table",
+      tableMD: {
+        columnNames: ["col"],
+        columnTypes: [
+          {
+            typeName: "INT",
+            isNullable: false,
+            maxLength: 0,
+            precision: 0,
+            scale: 0,
+          },
+        ],
+        isCTEnabled: true,
+        primaryKeyColumnCount: 1,
+      },
+    },
+  ];
+  const getTableIDString: spec.ComparisonOptions<string>["getTableIDString"] = (
+    s,
+  ) => s;
+  t.deepEqual(
+    spec.compareMetaData({
+      previousMetaData,
+      latestMetaData,
+      getTableIDString,
+    }),
+    {
+      ...previousMetaData,
+      "new-table": {
+        mdVersion: 0,
+        columns: {
+          col: previousMetaData["table"].columns["col"],
+        },
+      },
+    },
+    "Metadata must be considered different.",
+  );
+});
